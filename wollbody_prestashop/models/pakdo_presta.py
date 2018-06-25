888ef69426e44cc685114107037345e29d0718e1
ali <ali@ali-SSSD>
2018-06-25 19:36:35 +0530

    a

--- wollbody_prestashop/models/pakdo_presta.py
+++ wollbody_prestashop/models/pakdo_presta.py
@@ -77,7 +77,71 @@ class CSVPakdoPresta(models.TransientModel):
                     if product:
                         pp_stock.create({'product_id':product.id, 'gtin':gtin, 'qty':qty})
                         
-                    
-                    
-                    
+
+class PrestaPrice(models.Model):
+    _name = 'presta.price'
+    
+    product_id              = fields.Many2one('product.product','Product')
+    price                   = fields.Float('Impact Price')
+    default_code            = fields.Char('Internel Reference')
+    presta_child_id         = fields.Char(related='product_id.presta_child_id',string='Presta Child ID', store=True)
+    presta_id               = fields.Char(related='product_id.presta_id',string='Presta ID', store=True)
+    date_from               = fields.Date('Date From')
+    date_to                 = fields.Date('Date To')
+                        
+class CSVPrestaPrice(models.TransientModel):
+    _name = "csv.presta.price"
+    
+    file            = fields.Binary("File")
+    
+    
+    @api.multi
+    def update(self):
+        for record in self:
+            tmp_dir = tempfile.gettempdir()
+            f=open(tmp_dir+"/presta_price_import.csv",'w')
+            datafile=record.file
+            if datafile:
+                csv_data = base64.decodestring(datafile)
+            
+            f = StringIO(csv_data)
+            reader = csv.reader(f, delimiter=';')
+            cm = {}
+            for row in reader:
+                col_count = 0
+                for col in row:
+                    cm[col] = col_count
+                    col_count = col_count + 1
+                break;
+            
+
+            product_pool = self.env['product.product']
+            pp_stock_pool      = self.env['presta.price']
+            for row in reader:
                 
+                default_code         = row[0]
+                price                = row[1]
+                date_from            = row[2]
+                date_to              = row[3]
+                
+                
+                date_from = datetime.strptime(date_from,'%d/%m/%y %H:%M')
+                date_from_final = date_from.strftime("%m/%d/%Y")
+                
+                date_to = datetime.strptime(date_to,'%d/%m/%y %H:%M')
+                date_to_final = date_to.strftime("%m/%d/%Y")
+                
+
+                pp_stock = pp_stock_pool.search([('default_code','=',default_code)], limit=1)
+                if pp_stock:
+                    pp_stock.write({'price':price, 'date_from':date_from_final, 'date_to':date_to_final})
+                else:
+                    product = product_pool.search([('default_code','=',default_code)], limit=1)
+                    if product:
+                        pp_stock.create({'product_id':product.id, 'default_code':default_code, 'price':price, 'date_from':date_from_final, 'date_to':date_to_final})
+                        
+                        
+                        
+                        
+                        
+                        
