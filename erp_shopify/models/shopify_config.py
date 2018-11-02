cac12835301d402720cfdf9741ce288c0435fa29
ali <ali@ali-SSSD>
2018-11-02 12:00:58 +0530

    aa

--- erp_shopify/models/shopify_config.py
+++ erp_shopify/models/shopify_config.py
@@ -69,8 +69,10 @@ class ShopifyConfig(models.Model):
                 products = shopify_resp.Product().find(limit=250)
                 for product in products:
                     response_template=product.to_dict()
+                    
                     for variant in response_template.get('variants'):
-                        odoo_products = self.env['product.template'].search([('default_code','=',variant.get('sku'))], limit=1)
+                        print (variant)
+                        odoo_products = self.env['product.product'].search([('default_code','=',variant.get('sku'))], limit=1)
                         if odoo_products:
                             odoo_products.write({
                                                 'name'          : response_template.get('title') + ' - ' +variant.get('title'),
@@ -78,7 +80,7 @@ class ShopifyConfig(models.Model):
                                                 'shopify_id'    : variant.get('id'),
                                     })
                         if not odoo_products:
-                            odoo_products = self.env['product.template'].create({
+                            odoo_products = self.env['product.product'].create({
                                                 'name'          : response_template.get('title') +' - ' +variant.get('title'),
                                                 'type'          : 'product',
                                                 'shopify_id'    : variant.get('id'),
@@ -92,13 +94,110 @@ class ShopifyConfig(models.Model):
                 shopify_resp = shop.action_test_connection()
                 print ('>>>>>>>>>>      ',dir(shopify_resp))
                 orders = shopify_resp.Order().find(limit=250)
+                product_uom = self.env['product.uom.categ'].search([('name','=','Unit')])
                 for order in orders:
                     response_template = order.to_dict()
-                    print ('response_template      ',response_template)
+                    sale_order = self.env['sale.order'].search([('name','=',response_template.get('order_number')),('shopify_id','=',response_template.get('id'))])
+                    if not sale_order:
+                        partner_shipping = False
+                        partner = False
+                        customer_resp = response_template.get('customer')
+                        
+                        country = self.env['res.country'].search([('name','=',customer_resp['default_address'].get('country_name'))])
+                        partner_vals = {
+                                            'first_name'    : customer_resp.get('first_name'),
+                                            'last_name'     : customer_resp.get('last_name') if customer_resp.get('last_name') else '-',
+                                            'name'          : customer_resp.get('first_name') + ' ' + customer_resp.get('last_name') if customer_resp.get('last_name') else '-',
+                                            'email'         : customer_resp.get('email'),
+                                            'custom_company_name' : customer_resp['default_address'].get('company'),
+                                            'street2'       : customer_resp['default_address'].get('address1'),
+                                            'street'        : customer_resp['default_address'].get('address2'),
+                                            'city'          : customer_resp['default_address'].get('city'),
+                                            'zip'           : customer_resp['default_address'].get('zip'),
+                                            'phone'         : customer_resp['default_address'].get('phone'),
+                                            'country_id'    : country.id if country else False,
+                                            'customer'      : True,
+                                            'shopify_id'    : customer_resp.get('id'),
+                            
+                            }
+                        
+                        
+                        partner = self.env['res.partner'].search([('email','=',customer_resp.get('email')),('shopify_id','=',customer_resp.get('id'))])
+                        if partner:
+                            partner.write(partner_vals)
+                        if not partner:
+                            partner = self.env['res.partner'].create(partner_vals)
+                        
+                        if partner:
+                            country = self.env['res.country'].search([('name','=',response_template['shipping_address'].get('country_name'))])
+                            if not partner.street2 == response_template['shipping_address'].get('address1'):
+                                partner_shipping = self.env['res.partner'].search([('type','=','delivery'),('street2','=',response_template['shipping_address'].get('address1'))])
+                                if not partner_shipping:
+                                    shipping_vals = {
+                                                    'parent_id'     : partner.id,
+                                                    'type'          : 'delivery',
+                                                    'street2'       : response_template['shipping_address'].get('address1'),
+                                                    'street'        : response_template['shipping_address'].get('address2'),
+                                                    'city'          : response_template['shipping_address'].get('city'),
+                                                    'zip'           : response_template['shipping_address'].get('zip'),
+                                                    'phone'         : response_template['shipping_address'].get('phone'),
+                                                    'first_name'    : response_template['shipping_address'].get('first_name'),
+                                                    'last_name'     : response_template['shipping_address'].get('last_name') if response_template['shipping_address'].get('last_name') else '-',
+                                                    'name'          : response_template['shipping_address'].get('first_name') + ' ' + response_template['shipping_address'].get('last_name') if response_template['shipping_address'].get('last_name') else '-', 
+                                                    'country_id'    : country.id if country else False,
+                                        }
+                                    
+                                    partner_shipping = self.env['res.partner'].create(shipping_vals)
+                         
+                        sale_order = self.env['sale.order'].create({
+                                        'shopify_id'            : response_template.get('id'),
+                                        'partner_id'            : partner.id,
+                                        'partner_shipping_id'   : partner_shipping.id if partner_shipping else partner.id,
+                                        'partner_invoice_id'    : partner.id,
+                                        'name'                  : response_template.get('order_number'),
+                                        'date_order'            : response_template.get('created_at') 
+                            })
+                        
+                        for line in response_template.get('line_items'):
+                            if line.get('variant_id'):
+                                product = self.env['product.product'].search([('shopify_id','=',line.get('variant_id'))])
+                                if not product:
+                                    shop.action_import_products()
+                                    product = self.env['product.product'].search([('shopify_id','=',line.get('variant_id'))])
+                                    
+                                if product:
+                                    name = product.name_get()[0][1]
+                                    if product.description_sale:
+                                        name += '\n' + product.description_sale
+                                    sale_line = self.env['sale.order.line'].create({
+                                                        'order_id'          : sale_order.id,
+                                                        'product_id'        : product.id,
+                                                        'name'              : name,
+                                                        'product_uom_qty'   : line.get('quantity'),
+                                                        'price_unit'        : line.get('price'), 
+                                                        'product_uom'       : product_uom.id if product_uom else False  #unit
+                                        })
+                    
+                    
                     
                 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+    
+    shopify_id      = fields.Char(string='Shopify ID')
+
+class Product(models.Model):
+    _inherit = 'product.product'
+    
+    shopify_id      = fields.Char(string='Shopify ID')
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    shopify_id      = fields.Char(string='Shopify ID')
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
     
     shopify_id      = fields.Char(string='Shopify ID')
                     
