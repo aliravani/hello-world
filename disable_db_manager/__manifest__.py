c0c5ef2685784fe370f2fd84b6cbb75c21c226f5
ali <ali@ali-SSSD>
2018-11-06 18:45:50 +0530

    a

---
+++ disable_db_manager/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Disable Database Manager',
+    'version': '1.0',
+    'website': '',
+    'author': '',
+    'description': """
+    """,
+    'depends': ['base','web'],
+    'category': '',
+    'data': [
+         'views/template.xml',
+    ],
+    'qweb' : [],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
