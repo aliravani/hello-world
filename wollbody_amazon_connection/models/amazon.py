cc543d9b47be2406c83b34e13ca4bb3032ac9322
ali <ali@odoo.ie>
2019-01-16 17:51:37 +0530

    a

--- wollbody_amazon_connection/models/amazon.py
+++ wollbody_amazon_connection/models/amazon.py
@@ -736,7 +736,7 @@ class AmazonConfig(models.Model):
                     import_from = datetime.strptime(resp['last_update_date'], '%Y-%m-%d %H:%M:%S')
                     _logger.info('Amazon : last_update_date........  ' + str(type(import_from)))
                     
-                request = {'Action': 'ListOrders', 'LastUpdatedAfter'   : import_from.strftime('%Y-%m-%dT%H:%M:%S')}
+                request = {'Action': 'ListOrders', 'LastUpdatedAfter'   : import_from.strftime('%Y-%m-%dT%H:%M:%SZ')}
                 request.update(market_places)
                 bag =  self.make_request(request, section='/Orders/2013-09-01', version='2013-09-01')
 
@@ -768,8 +768,8 @@ class AmazonConfig(models.Model):
                         order_id = order_ids[0] 
                     else:
                         
-                        last = datetime.strptime(order.LastUpdateDate, '%Y-%m-%dT%H:%M:%SZ')
-                        #last = datetime.strptime(order.LastUpdateDate, '%Y-%m-%dT%H:%M:%S')
+                        #last = datetime.strptime(order.LastUpdateDate, '%Y-%m-%dT%H:%M:%SZ')
+                        last = datetime.strptime(order.LastUpdateDate, '%Y-%m-%dT%H:%M:%S')
                         last = last.strftime('%Y-%m-%d %H:%M:%S')
                         #last = order.LastUpdateDate.strftime('%Y-%m-%dT%H:%M:%SZ')
                         #print '>>>>>>>>>>>>>llllllllll        ',last
